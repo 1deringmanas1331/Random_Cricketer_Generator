6427f37a358679f65edb07b90585e6594b8cfabd
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2023-11-24 05:47:30 +0530

    Random_Cricketer_generator.py

--- Random_Cricketer_generator.py
+++ Random_Cricketer_generator.py
@@ -9,6 +9,7 @@ last_names = ["Kohli", "Tendulkar", "Sharma", "Dhoni", "Williamson", "Root", "Sm
 random_first_name = random.choice(first_names)
 random_last_name = random.choice(last_names)
 
+# Combines the first and last name to generate the cricketer's name
 random_cricketer_name = f"{random_first_name} {random_last_name}"
 
 print(random_cricketer_name)
